2be06cec57fa26ed580064fd628a88362d9d89aa
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-01-08 23:40:44 -0500

    feat: created commit data model

--- data/repositories/CommitRepository.ts
+++ data/repositories/CommitRepository.ts
@@ -1,3 +1,16 @@
+// data type for a commit
+type Commit = {
+    commitId: string;
+    name: string;
+    type: string;
+    date: string;
+    subject: string;
+    body: string;
+    message: string;
+    pageId?: string;
+}
+    
+
 // class for commit repository
 class CommitRepository {
   constructor(){
