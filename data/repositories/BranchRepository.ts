6c5dddb05ca144c3244dc3a5c7d1ea8f08bf05fb
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-01-08 21:54:31 -0500

    feat: added ability to create a branch page entry

--- data/repositories/BranchRepository.ts
+++ data/repositories/BranchRepository.ts
@@ -1,3 +1,6 @@
+import notion from "../../utils/notion";
+import { BranchFields, RelationshipFields } from "../enums";
+
 type Branch = {
     branchId: string;
     name: string;
@@ -10,11 +13,83 @@ type Branch = {
     // repository: string;
 }
 
+const createBranchEntry = async (branch: Omit<Branch, "pageId">) => {
+    const response = await notion.pages.create({
+        parent: {
+            database_id: RelationshipFields.Branches,
+        },
+        properties: {
+            [BranchFields.BranchId]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: branch.branchId,
+                        },
+                    },
+                ],
+            },
+            [BranchFields.Name]: {
+                type: "title",
+                title: [
+                    {
+                        type: "text",
+                        text: {
+                            content: branch.name,
+                        },
+                    },
+                ],
+            },
+            [BranchFields.Path]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: branch.path,
+                        },
+                    },
+                ],
+            },
+            [BranchFields.BranchCreatedOn]: {
+                type: "date",
+                date: {
+                    start: branch.branchCreatedOn,
+                }
+            },
+            [BranchFields.RemovedOn]: {
+                type: "date",
+                date: {
+                    start: branch.removedOn,
+                }
+            },
+            [BranchFields.Status]: {
+                type: "status",
+                status: {
+                    id: branch.status,
+                }
+            },
+        }
+    });
+};
+
 class BranchRepository {
-  constructor(){
+    constructor() {
+
+    }
+
     
-  }
-  
+
+    public async createBranch(branch: Branch): Promise<boolean> {
+        try {
+            await createBranchEntry(branch);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
 }
 
 export default BranchRepository
