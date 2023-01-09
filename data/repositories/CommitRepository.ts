c3555fd315eca8d66b556b620bb89daf813e87e5
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-01-09 00:02:50 -0500

    feat: added ability to create a commit entry

--- data/repositories/CommitRepository.ts
+++ data/repositories/CommitRepository.ts
@@ -1,3 +1,9 @@
+// import notion client
+import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import notion, { notionPropertiesById } from "../../utils/notion";
+import { CommitFields, RelationshipFields } from "../enums";
+
+
 // data type for a commit
 type Commit = {
     commitId: string;
@@ -9,14 +15,121 @@ type Commit = {
     message: string;
     pageId?: string;
 }
-    
+
+// create a commit entry
+const createCommitEntry = async (commit: Omit<Commit, "pageId">) => {
+    const response = await notion.pages.create({
+        parent: {
+            database_id: RelationshipFields.Commits,
+        },
+        properties: {
+            [CommitFields.CommitId]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: commit.commitId,
+                        },
+                    },
+                ],
+            },
+            [CommitFields.Name]: {
+                type: "title",
+                title: [
+                    {
+                        type: "text",
+                        text: {
+                            content: commit.name,
+                        },
+                    },
+                ],
+            },
+            [CommitFields.Type]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: commit.type,
+                        },
+                    },
+                ],
+            },
+            [CommitFields.Date]: {
+                type: "date",
+                date: {
+                    start: commit.date,
+                },
+            },
+            [CommitFields.Subject]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: commit.subject,
+                        },
+                    },
+                ],
+            },
+            [CommitFields.Body]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: commit.body,
+                        },
+                    },
+                ],
+            },
+            // [CommitFields.Message]: {
+            //     type: "rich_text",
+            //     rich_text: [
+            //         {
+            //             type: "text",
+            //             text: {
+            //                 content: commit.message,
+            //             },
+            //         },
+            //     ],
+            // },
+        },
+    });
+
+    return response;
+}
+
 
 // class for commit repository
 class CommitRepository {
-  constructor(){
+    constructor() {
+
+    }
+
+    // get a commit by id
+
+    // get all commits
+
+    // get commits by branch id
+
+    // get commits by repository id
     
-  }
-  
+    // get commits by user id
+
+    // create a commit
+    public async createCommit(commit: Omit<Commit, "pageId">) {
+        try {
+            await createCommitEntry(commit);
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+
+        }
+    }
+
 }
 
 export default CommitRepository
