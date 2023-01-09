990c42693fb2423ba97fc89695cefe6684f1bb57
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-01-08 21:17:53 -0500

    feat: transformed field to be referenced by id

--- utils/notion.ts
+++ utils/notion.ts
@@ -1,4 +1,5 @@
 import { Client } from "@notionhq/client";
+import { CreatePageParameters, PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 const notion = new Client({
     auth: process.env.NOTION_SECRET,
@@ -11,6 +12,21 @@ export const listBlocks = async (pageId: string) => {
     });
     return { response, results: response.results };
 };
+
+// type CreateDatabaseProperties = Pick<CreatePageParameters, "properties">;
+
+type NotionIdProperty = { [index: string]: Omit<PageObjectResponse["properties"], "id"> };
+
+
+
+export const notionPropertiesById = (
+    properties: PageObjectResponse["properties"]
+): Record<string, Omit<PageObjectResponse["properties"], "id">[string]> => {
+    return Object.values(properties).reduce((obj, property) => {
+        const { id, ...rest } = property;
+        return { ...obj, [id]: rest };
+    }, {});
+}
 
 export const getDatabase = async (databaseId: string) => {
     const response = await notion.databases.retrieve({
