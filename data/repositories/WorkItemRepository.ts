fc9285b534a0979b324c65efa42915f8d6725aee
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-01-08 22:21:41 -0500

    feat: created work item data model

--- data/repositories/WorkItemRepository.ts
+++ data/repositories/WorkItemRepository.ts
@@ -1,3 +1,17 @@
+// data type for a work item
+type WorkItem = {
+    workItemId: string;
+    name: string;
+    type: string;
+    size: string;
+    tags: string;
+    description: string;
+    acceptanceCriteria: string;
+    state: string;
+    points: string;
+    pageId?: string;
+};
+
 // class for work item repository
 class WorkItemRepository {
   constructor(){
