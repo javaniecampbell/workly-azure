5150631310f80dda936dff86f3028e267898a4d4
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-01-08 22:40:01 -0500

    feat: added ability to create a workitem entry

--- data/repositories/WorkItemRepository.ts
+++ data/repositories/WorkItemRepository.ts
@@ -1,3 +1,7 @@
+// import notion client
+import notion, { notionPropertiesById } from "../../utils/notion";
+import { WorkItemFields, RelationshipFields } from "../enums";
+
 // data type for a work item
 type WorkItem = {
     workItemId: string;
@@ -12,12 +16,114 @@ type WorkItem = {
     pageId?: string;
 };
 
+// create a work item entry
+const createWorkItemEntry = async (workItem: Omit<WorkItem, "pageId">) => {
+    const response = await notion.pages.create({
+        parent: {
+            database_id: RelationshipFields.WorkItems,
+        },
+        properties: {
+            [WorkItemFields.WorkItemId]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: workItem.workItemId,
+                        },
+                    },
+                ],
+            },
+            [WorkItemFields.Name]: {
+                type: "title",
+                title: [
+                    {
+                        type: "text",
+                        text: {
+                            content: workItem.name,
+                        },
+                    },
+                ],
+            },
+            [WorkItemFields.Type]: {
+                type: "select",
+                select: {
+                    id: workItem.type,
+                }
+            },
+            [WorkItemFields.Size]: {
+                type: "select",
+                select: {
+                    id: workItem.size,
+                }
+            },
+            [WorkItemFields.Tags]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: workItem.tags,
+                        },
+                    },
+                ],
+            },
+            [WorkItemFields.Description]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: workItem.description,
+                        },
+                    },
+                ],
+            },
+            [WorkItemFields.AcceptanceCriteria]: {
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: workItem.acceptanceCriteria,
+                        },
+                    },
+                ],
+            },
+            // [WorkItemFields.State]: {
+            //     type: "rich_text",
+            //     rich_text: [
+            //         {
+            //             type: "text",
+            //             text: {
+            //                 content: workItem.state,
+            //             },
+            //         },
+            //     ],
+            // }
+        },
+    });
+    return response;
+};
+
 // class for work item repository
 class WorkItemRepository {
-  constructor(){
+    constructor() {
+
+    }
+
+    // create a work item return true if successful, false otherwise
+    async createWorkItem(workItem: Omit<WorkItem, "pageId">): Promise<boolean> {
+        try {
+            await createWorkItemEntry(workItem);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
     
-  }
-  
+
 }
 
 export default WorkItemRepository
