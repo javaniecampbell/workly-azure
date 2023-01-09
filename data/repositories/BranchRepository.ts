0d0494da4513964a5970077a48176dd6fd3c83f2
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-01-08 21:32:28 -0500

    feat:  created branch data model

--- data/repositories/BranchRepository.ts
+++ data/repositories/BranchRepository.ts
@@ -1,3 +1,15 @@
+type Branch = {
+    branchId: string;
+    name: string;
+    path: string;
+    branchCreatedOn: string;
+    removedOn: string;
+    status: string;
+    pageId?: string;
+    // commits: string;
+    // repository: string;
+}
+
 class BranchRepository {
   constructor(){
     
